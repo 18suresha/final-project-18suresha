b7f4e6d00085c34e120bbcbc6a803118c100e163
Adarsh Suresh <adarshs3@illinois.edu>
2020-05-03 22:10:20 -0500

    Added neutral zone tests

--- test-final-project/test.cc
+++ test-final-project/test.cc
@@ -3,14 +3,59 @@
 #define CATCH_CONFIG_MAIN
 
 #include "catch.hpp"
-#include "opencv2/opencv.hpp"
+#include "opencv2/core/types.hpp"
 #include "mylibrary/engine.h"
 
 using mylibrary::Engine;
+using cv::Size;
+using cv::Rect;
 
 
 TEST_CASE("Get Camera Size") {
     Engine engine{};
-    cv::Size cam_size = engine.GetCamFrameSize();
-    REQUIRE(cam_size == cv::Size{ 640, 480 });
+    Size cam_size = engine.GetCamFrameSize();
+    REQUIRE(cam_size == Size{ 640, 480 });
+}
+
+TEST_CASE("Neutral Zones") {
+    Engine engine{};
+    Size cam_size = engine.GetCamFrameSize();
+    SECTION("Default Neutral Zone") {
+        Rect neutral_zone = engine.GetNeutralZone();
+        REQUIRE(neutral_zone == Rect{ 213, 200, 186, 120 });
+        REQUIRE(engine.IsNeutralZoneValid());
+    }
+    SECTION("Custom Neutral Zone") {
+        engine.SetNeutralZone(Rect{ 0, 0, 640, 480 });
+        Rect neutral_zone = engine.GetNeutralZone();
+        REQUIRE(neutral_zone == Rect{ 0, 0, 640, 480 });
+        REQUIRE(engine.IsNeutralZoneValid());
+    }
+    SECTION("Neutral Zone with Invalid X Starting Point") {
+        engine.SetNeutralZone(Rect{ -1, 0, 640, 480 });
+        Rect neutral_zone = engine.GetNeutralZone();
+        REQUIRE(neutral_zone == Rect{ -1, 0, 640, 480 });
+        REQUIRE(!engine.IsNeutralZoneValid());
+    }
+
+    SECTION("Neutral Zone with Invalid Y Starting Point") {
+        engine.SetNeutralZone(Rect{ 0, -1, 640, 480 });
+        Rect neutral_zone = engine.GetNeutralZone();
+        REQUIRE(neutral_zone == Rect{ 0, -1, 640, 480 });
+        REQUIRE(!engine.IsNeutralZoneValid());
+    }
+
+    SECTION("Neutral Zone with Invalid Width") {
+        engine.SetNeutralZone(Rect{ 0, 0, 641, 480 });
+        Rect neutral_zone = engine.GetNeutralZone();
+        REQUIRE(neutral_zone == Rect{ 0, 0, 641, 480 });
+        REQUIRE(!engine.IsNeutralZoneValid());
+    }
+
+    SECTION("Neutral Zone with Invalid Height") {
+        engine.SetNeutralZone(Rect{ 0, 0, 640, 481 });
+        Rect neutral_zone = engine.GetNeutralZone();
+        REQUIRE(neutral_zone == Rect{ 0, 0, 640, 481 });
+        REQUIRE(!engine.IsNeutralZoneValid());
+    }
 }
